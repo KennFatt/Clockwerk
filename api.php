b21191bbcda4836eb9ae71c9f5c25ab251cf1e68
KennFatt <clashofklen@gmail.com>
2018-10-01 21:39:20 +0700

    Adding new file api.php

---
+++ api.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Clockwerk - RESTful API for your life.
+ * Light, fast, and easy to use.
+ * 
+ * @author KennFatt <me@kennan.xyz>
+ * @link https://kennan.xyz/
+ * @copyright 2018 KennFatt - Clockwerk
+ */
+
+declare(strict_types=1);
+
+/**
+ * Work directory
+ */
+define("ROOT_DIR", __DIR__);
