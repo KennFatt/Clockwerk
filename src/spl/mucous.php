1af7437b46feaf5f8360ee515c1d76ab71f9d9f2
KennFatt <clashofklen@gmail.com>
2019-03-09 19:16:45 +0700

    Project take over...

--- src/spl/mucous.php
+++ src/spl/mucous.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Clockwerk - RESTful API for your life.
+ * Clockwerk - REST API for your life.
  * Light, fast, and easy to use.
- * 
+ *
  * @author KennFatt <me@kennan.xyz>
  * @link https://kennan.xyz/
- * @copyright 2018 KennFatt - Clockwerk
+ * @copyright 2019 - Clockwerk
  */
 
 declare(strict_types=1);
